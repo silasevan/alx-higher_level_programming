ff7b802c0e84377a86f6d2f5ab6f22d579fdd090
silasevan <silasevan@gmail.com>
2022-06-08 09:08:24 -0700

    search

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 
-def search_replace(my_list, search, replace):
+# 1-search_replace.py
 
-    new_list = [replace if i == search else i for i in my_list]
+
+def search_replace(my_list, search, replace):
+    """Replace all occurrences of an element by another in a new list."""
+    new_list = my_list[:]
+    for i in range(len(new_list)):
+        if new_list[i] == search:
+            new_list[i] = replace
+            return (new_list)
 
