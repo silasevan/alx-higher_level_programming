4da92432761dd3c72049b09dd528cec80a712601
silasevan <silasevan@gmail.com>
2023-06-13 13:16:36 -0700

    concat

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,7 +1,3 @@
 #!/usr/bin/node
 
-const argument1 = process.argv[2];
-const argument2 = process.argv[3];
-
-console.log(argument1 + " is " + argument2);
-
+console.log(process.argv[2] + ' is ' + process.argv[3]);
