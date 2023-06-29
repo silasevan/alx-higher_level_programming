3b356ca81d010a11eabf30d83c649421f89e05d1
silasevan <silasevan@gmail.com>
2023-06-29 13:23:21 -0700

    task0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import urllib.request
+
+url = 'https://alx-intranet.hbtn.io/status'
+
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+
+    print("Body response:")
+    print("    - type:", type(body))
+    print("    - content:", body)
+    print("    - utf8 content:", body.decode('utf-8'))
+
