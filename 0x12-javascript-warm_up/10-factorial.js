dbbf188076d09e1b76a87ffd59e108e7f75fe4ae
silasevan <silasevan@gmail.com>
2024-06-19 14:30:33 +0100

    second_biggest

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,6 @@
 #!/usr/bin/node
-
-function factorial(n) {
-  if (isNaN(n) || n === 0) {
-    return 1;
-  }
-  return n * factorial(n - 1);
+function factorial (n) {
+  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
 }
 
-const arg = process.argv[2];
-const number = parseInt(arg, 10);
-console.log(factorial(number));
+console.log(factorial(Number(process.argv[2])));
